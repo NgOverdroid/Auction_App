a35e63cb097ed0b4019b2fd98efa67342840f964
Duc Nghia <21521180@gm.uit.edu.vn>
2025-03-12 15:08:04 +0700

    added prisma adapters

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,12 +1,17 @@
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { auth } from "../auth";
+import { SignIn } from "@/components/signin";
+import { SignOut } from "@/components/signout";
 
 export default async function HomePage() {
-  // const bids = await prisma.bids.findMany();
+  const bids = await prisma.bids.findMany();
+  const session = await auth();
 
   return (
       <main className="container mx-auto py-12 ">
+        { session ? <SignOut/> : <SignIn/>}
         <form>
           <Input type="text" name="bid" />
           <Button type="submit">Create a Bid</Button>
